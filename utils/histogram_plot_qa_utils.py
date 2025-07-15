d7d02638d3336e9e67060d54d3471f077c316ed9
jnaiman <jnaiman.illinois.edu>
2025-07-14 19:26:54 -0500

    better qa pairs

--- utils/histogram_plot_qa_utils.py
+++ utils/histogram_plot_qa_utils.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from utils.plot_qa_utils import plot_index_to_words
 
-#q_stats_hists
 #q_gmm_ngaussians_hists
 
 def get_nplots(data):
@@ -24,7 +23,7 @@ def persona(text=None):
     return text
 
 
-def context(nrow, ncol, plot_index = 0, 
+def context(nrow, ncol, plot_index = [0,0], 
             use_words=True, single_figure_flag=True):
     """
     This sets the context of the question by flagging what panel of 
@@ -57,8 +56,15 @@ def context(nrow, ncol, plot_index = 0,
 
 ##### FEEDER FUNCTIONS ######
 
-def how_many(object, big_tag, val_type = 'an integer', nplots = 1, use_words=True):
-    q = 'How many '+object+' are there in the specified figure panel?'
+def how_many(object, big_tag, val_type = 'an integer', nplots = 1, 
+             use_words=True, to_generate=False):
+    """
+    to_generate : flag for wording
+    """
+    if not to_generate:
+        q = 'How many '+object+' are there in the specified figure panel?'
+    else:
+        q = 'How many ' + object + ' were used to generate the data for the plot in the figure panel?'
     if use_words and nplots > 1:
         adder = '(words)'
     elif not use_words and nplots > 1:
@@ -82,10 +88,31 @@ def how_much_data_values(big_tag, nplots=1, axis='x', val_type='a float', use_wo
     format += 'data values used to create the plot.'
     return q, adder, format
 
+
+def check_relationship(data, plot_num, qa_pairs, rel = 'gmm',
+                       return_qa = True, verbose=False):
+    # is this the correct relationship? if not this is an error
+    hasRel= False
+    #rel = 'gmm'
+    if data['plot'+str(plot_num)]['distribution'] != rel: # not a linear relationship
+        if verbose:
+            print('Not a '+rel+' relationship!')
+        if return_qa:
+            return qa_pairs, hasRel
+        else:
+            return None, hasRel
+
+    if data['plot'+str(plot_num)]['distribution'] == rel:
+        hasRel = True
+
+    if not hasRel:
+        print("This is not a '"+rel+"' relationship, can't ask any questions!'")
+        import sys; sys.exit()
+    return hasRel, hasRel
 ####### CONSTRUCT QUESTIONS ########
 
 # this version tries to give column and row numbers
-def q_nbars_hist_plot_plotnums(data, qa_pairs, plot_num = 0, 
+def q_nbars_hist_plot_plotnums(data, qa_pairs, plot_num = [0,0], 
                                return_qa=True, verbose=True, use_words=True, 
                                single_figure_flag=True, 
                                text_persona = None):
@@ -135,7 +162,8 @@ def q_nbars_hist_plot_plotnums(data, qa_pairs, plot_num = 0,
 
 def q_stats_hists(data, qa_pairs, stat = {'minimum':np.min}, plot_num = 0, 
                      return_qa=True, use_words=True, verbose=True, 
-                     single_figure_flag=True):
+                     single_figure_flag=True, 
+                               text_persona = None):
     """
     stat: {'name':stat} which gives name of stat and function to calculate it, like {'minimum':np.min}
     use_words : set to True to translate row, column to words; False will use C-ordering indexing
@@ -184,6 +212,70 @@ def q_stats_hists(data, qa_pairs, stat = {'minimum':np.min}, plot_num = 0,
                                                                                                         'format':text_format}}
         else:
             qa_pairs['Level 2']['Plot-level questions'][big_tag + ' ' + adder]['plot'+str(plot_num)] = {'Q':q, 'A':a, 
+                                                                                                        'persona':text_persona, 
+                                                                                                        'context':text_context,
+                                                                                                        'question':text_question, 
+                                                                                                        'format':text_format}
+        return qa_pairs
+    
+
+def q_gmm_ngaussians_hists(data, qa_pairs, plot_num = 0, 
+                           return_qa=True, use_words=True, verbose=True, 
+                           single_figure_flag = True, 
+                               text_persona = None):
+    """
+    use_words : set to True to translate row, column to words; False will use C-ordering indexing
+    use_nlines : give the number of lines in the prompt
+    """
+
+    # check correct relationship
+    hasRel, qatmp = check_relationship(data, plot_num, qa_pairs, rel = 'gmm',
+                       return_qa = return_qa, verbose=verbose)
+    if not hasRel:
+        return qatmp
+
+    big_tag = 'ngaussians'
+    object = 'gaussians'
+    nplots = get_nplots(data)
+
+    ### persona of assistant
+    text_persona = persona(text=text_persona)
+    ## context for question
+    if nplots == 1 and single_figure_flag:
+        text_context = context(0, 0, use_words=use_words,
+                                single_figure_flag=single_figure_flag)
+    else:
+        nrow = data['figure']['plot indexes'][plot_num][0]
+        ncol = data['figure']['plot indexes'][plot_num][1]
+        pindex = data['figure']['plot indexes'][plot_num]
+        text_context = context(nrow,ncol,plot_index=pindex, use_words=use_words)
+
+    ### question, format of output
+    text_question, adder, text_format = how_many(object, big_tag, 
+                                                       val_type = 'an integer', 
+                                                       nplots = nplots, 
+                                                       use_words=use_words, 
+                                                       to_generate=True)
+
+    ### answer
+    la = data['plot'+str(plot_num)]['data']['data params']['nclusters']
+
+    a = {big_tag + ' ' + adder:la}
+    # construct question:
+    q = text_persona + " " + text_context + " " + text_question + " " + text_format
+
+    if verbose:
+        print('QUESTION:', q)
+        print('ANSWER:', {'plot'+str(plot_num):a})
+    if return_qa: 
+        if big_tag + ' ' + adder not in qa_pairs['Level 3']['Plot-level questions']:
+            qa_pairs['Level 3']['Plot-level questions'][big_tag + ' ' + adder] = {'plot'+str(plot_num):{'Q':q, 'A':a, 
+                                                                                                        'persona':text_persona, 
+                                                                                                        'context':text_context,
+                                                                                                        'question':text_question, 
+                                                                                                        'format':text_format}}
+        else:
+            qa_pairs['Level 3']['Plot-level questions'][big_tag + ' ' + adder]['plot'+str(plot_num)] = {'Q':q, 'A':a, 
                                                                                                         'persona':text_persona, 
                                                                                                         'context':text_context,
                                                                                                         'question':text_question, 
