1737bc9e6c9c500a24a147f7be68dae951f826bf
jnaiman <email>
2025-07-24 11:43:33 -0500

    hist

--- utils/plot_classes_utils.py
+++ utils/plot_classes_utils.py
@@ -14,8 +14,11 @@ class Histogram():
 
 class Line():
     def __init__(self):
-        self.rwidth = None
+        self.hasMarker = None
         self.elinewidth = None
+        self.marker = None
+
+        self.rwidth = None
         self.orientation = None
         self.axis = None
         self.lthick = None
